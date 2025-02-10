2cd08fbe56ffb0f919a78420c6c0f05f63366831
Kousik27-23 <kousikghadai@gmail.com>
2025-02-10 15:23:36 +0530

    Add files via upload

---
+++ geekforgeeks/p25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num,k=1;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    for (int i = 0; i <= num; i++)
+    {
+        k+=i;
+    }
+    
+
+
+return 0;
+}
