137847533770e7bc22cef5b5f571e0c5b020915a
Kousik27-23 <kousikghadai@gmail.com>
2025-02-16 16:56:52 +0530

    Delete geekforgeeks directory

--- geekforgeeks/p2.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-printf("kousik ghadai");
-return 0;
-}
--- geekforgeeks/p7.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char ch;
-printf("Enter character: ");
-scanf("%c",&ch);
-printf("ASCII value is %c=%d",ch,ch);
-return 0;
-}
